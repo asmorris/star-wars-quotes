ddc568ed3ea1d6ce35ecd9b3b50d30755be41d28
Andrew Morrison <morro141@gmail.com>
2016-11-20 16:46:36 -0500

    Add ability to delete Darth Vader quotes

--- server.js
+++ server.js
@@ -49,3 +49,11 @@ app.put('/quotes', (req,res) => {
 		res.send(result)
 	})
 })
+
+app.delete('/quotes', (req, res) => {
+	database.collection('quotes').findOneAndDelete({name: req.body.name},
+	(err, result) => {
+		if (err) return res.send(500, err)
+		res.send('A darth vader quote was deleted')
+	})
+})
